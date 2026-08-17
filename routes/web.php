b0eaa37816b9d0e9ccc7c705eee5d7e0a89336c7
NanaT <samuelagyekumhene@gmail.com>
2026-08-17 12:46:37 +0100

    updates to contact route

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
 
-Route::post('/contact-submit', [HomeController::class, 'submitContact'])->name('contact.submit');
+Route::post('/send-message', [HomeController::class, 'submitContact'])->name('contact.submit');
 
 Route::get('/contact-submit', function () {
     return 'Laravel reached';
